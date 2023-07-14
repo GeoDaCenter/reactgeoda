f847a9c4c4f2121bf97ebe7e21d0a94d2b7d5beb
justin-kleid <jkleidermacher@uchicago.edu>
2023-07-13 23:46:16 -0500

    Kepler: remove panel and file input

--- store/index.js
+++ store/index.js
@@ -1,10 +1,25 @@
-import keplerGlReducer from "kepler.gl/reducers";
+import keplerGlReducer, { uiStateUpdaters } from "kepler.gl/reducers";
 import {createStore, combineReducers, applyMiddleware} from "redux";
 import {taskMiddleware} from "react-palm/tasks";
 import rootReducer from '../reducers/index'; 
 
+const customizedKeplerGlReducer = keplerGlReducer
+  .initialState({
+    uiState: {
+      // hide side panel and data input window to disallow user customize the map
+      readOnly: true,
+      currentModal: null,
+      mapLegend: {
+        show: true,
+        active: true
+      },
+    }
+  });
+
+
+
 const reducers = combineReducers({
-  keplerGl: keplerGlReducer,
+  keplerGl: customizedKeplerGlReducer,
   root: rootReducer,
 });
 
