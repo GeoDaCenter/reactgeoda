f329c0f86126a56b755528ada79355deb7e4ea0b
Xun Li <lixun910@gmail.com>
2023-07-19 22:27:29 -0700

    add eslint and prettier (#12)

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: ['eslint-plugin-prettier'],
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:css/recommended',
+    // Add "prettier" last. This will turn off eslint rules conflicting with prettier. This is not what will format our code.
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+};
