efcbf2b4b8ce0eb4ec4b32c5b1c2466203163158
justin-kleid <jkleidermacher@uchicago.edu>
2023-07-25 10:32:28 -0500

    Squashed commit of the following:
    commit 4e450524d160dc1dbe7b8a5c46df4b80031e5f72
    Author: Justin Kleidermacher <105233990+justin-kleid@users.noreply.github.com>
    Date:   Thu Jul 20 20:29:07 2023 -0500
    
        Organized files with component directory (#7)
    
        * Created Components directory
    
        * Deleted page.zip
    
        * Just added a package
    
        * Customize kepler UI (#8)
    
        * Kepler: remove panel and file input
    
        * Internationalize kepler (#9)
    
        * Had kepler change languages with rest of app
    
        * Toolbar Graphing Basics (#10)
    
        * Enabled basic graphing features via toolbar
    
        * Internationalize toolbar (#11)
    
        * internationalized toolbar + make it prettier
    
        * Load geojson (#15)
    
        * initialize jsgeoda component + example
    
        * Load geojson. Process csv + geojson w kepler utils
    
        * load shapefiles using browserfilesystem--> geojson (#16)
    
    commit 65965616df63186332bb47299b49360b4a989516
    Author: justin-kleid <jkleidermacher@uchicago.edu>
    Date:   Thu Jul 13 22:46:29 2023 -0500
    
        Delete test folder
    
    commit 0682c1c4124fc19e73bc66363b474b0c3ad52633
    Author: justin-kleid <jkleidermacher@uchicago.edu>
    Date:   Thu Jul 13 22:42:13 2023 -0500
    
        Used redux to store the drag n drop data
    
    commit 69ce733892f8a96e31f4aca25462e533cffac0b4
    Author: justin-kleid <jkleidermacher@uchicago.edu>
    Date:   Thu Jul 13 21:01:01 2023 -0500
    
        Added translations for file drop and chatgpt
    
    commit 31128ef47b18c7e59fcf49a484cf10bb3fe3033f
    Author: justin-kleid <jkleidermacher@uchicago.edu>
    Date:   Thu Jul 13 19:19:25 2023 -0500
    
        Add translations folder and import react-intl

--- store/index.js
+++ store/index.js
@@ -1,12 +1,29 @@
-import keplerGlReducer from "kepler.gl/reducers";
-import {createStore, combineReducers, applyMiddleware} from "redux";
-import {taskMiddleware} from "react-palm/tasks";
-import rootReducer from '../reducers/index'; 
+import keplerGlReducer, { uiStateUpdaters } from "kepler.gl/reducers";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { taskMiddleware } from "react-palm/tasks";
+import rootReducer from "../reducers/index";
+import keplerLanguageMiddleware from "./keplerLanguageMiddleware";
+
+const customizedKeplerGlReducer = keplerGlReducer.initialState({
+  uiState: {
+    // hide side panel and data input window to disallow user customize the map
+    readOnly: true,
+    currentModal: null,
+    mapLegend: {
+      show: true,
+      active: true,
+    },
+  },
+});
 
 const reducers = combineReducers({
-  keplerGl: keplerGlReducer,
+  keplerGl: customizedKeplerGlReducer,
   root: rootReducer,
 });
 
-const store = createStore(reducers, {}, applyMiddleware(taskMiddleware));
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(taskMiddleware, keplerLanguageMiddleware)
+);
 export default store;
