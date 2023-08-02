8ecb03c413809482182e6459c4041d9defd4b476
Xun Li <lixun910@gmail.com>
2023-08-02 11:06:54 -0700

    add build-prod

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
-const isProduction = process.env.NODE_ENV !== 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
   basePath: process.env.BASE_PATH ?? '/reactgeoda',
-  images: {
-    unoptimized: true
-  },
+  images: {unoptimized: true},
   generateBuildId: async () => 'reactgeoda-v0.1',
   webpack: config => {
     config.optimization.splitChunks = false;
