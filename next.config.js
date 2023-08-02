184f90125d83c59bdaa07e528c75cdb8c63f5296
Xun Li <lixun910@gmail.com>
2023-08-02 10:36:44 -0700

    add BASE_PATH and NODE_ENV

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
-/** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV !== 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
-  basePath: '/reactgeoda',
+  basePath: process.env.BASE_PATH ?? '/reactgeoda',
   images: {
     unoptimized: true
   },
-  entry: './pages/map.js',
+  generateBuildId: async () => {
+    return 'reactgeoda-v0.1';
+  },
   webpack: config => {
     config.optimization.splitChunks = false;
+    config.optimization.minimize = isProduction;
     return config;
   }
 };
