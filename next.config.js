8c60a2d0d96b95c32d09cf1730d85fa2e5837ad9
Xun Li <lixun910@gmail.com>
2023-08-02 11:16:48 -0700

    Misc: fix depoly action from main branch (#27)

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
-/** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
-  basePath: '/reactgeoda',
-  images: {
-    unoptimized: true
-  },
-  entry: './pages/map.js',
+  basePath: process.env.BASE_PATH ?? '/reactgeoda',
+  images: {unoptimized: true},
+  generateBuildId: async () => 'reactgeoda-v0.1',
   webpack: config => {
     config.optimization.splitChunks = false;
+    config.optimization.minimize = isProduction;
     return config;
   }
 };
