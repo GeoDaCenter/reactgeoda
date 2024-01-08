25729ea36beebc55cb01de25a93d02a939f98f26
Xun Li <lixun910@gmail.com>
2024-01-07 21:16:25 -0700

    update settings panel

--- geoda-ai/src/components/panel/panel-container.tsx
+++ geoda-ai/src/components/panel/panel-container.tsx
@@ -3,6 +3,8 @@ import {useSelector} from 'react-redux';
 import {GeoDaState} from '../../store';
 import {ChatGptComponent} from '../chatgpt/chatgpt-wrapper';
 import {SettingsPanel} from './settings-panel';
+import {IconXClose} from '../icons/xclose';
+import '../../styles/settings-panel.css';
 
 export const PanelContainer = () => {
   // get showGridView from redux state
@@ -10,14 +12,18 @@ export const PanelContainer = () => {
     (state: GeoDaState) => state.root.uiState.showPropertyPanel
   );
 
-
-  const showChatGPTPanel = false;
-  const showSettingsPanel = true;
+  const showChatGPTPanel = true;
+  const showSettingsPanel = false;
 
   return showPropertyPanel ? (
     <div className="prop-box">
-      {showChatGPTPanel && <ChatGptComponent />}
-      {showSettingsPanel && <SettingsPanel />}
+      <div className="button-close-x">
+        <IconXClose className="x-close" />
+      </div>
+      <div className="prop-box-content">
+        {showChatGPTPanel && <ChatGptComponent />}
+        {showSettingsPanel && <SettingsPanel />}
+      </div>
     </div>
   ) : null;
 };
