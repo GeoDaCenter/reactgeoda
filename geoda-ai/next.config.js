b329b16d833eee583ebb1c4c4c7f0c53f6828ebc
Xun Li <lixun910@gmail.com>
2024-01-10 17:55:46 -0700

    fix for netlify

--- geoda-ai/next.config.js
+++ geoda-ai/next.config.js
@@ -3,6 +3,8 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  target: 'serverless',
   basePath: process.env.BASE_PATH ?? '/reactgeoda',
   typescript: {
     // !! WARN !! This is to ignore build errors from Kepler.gl
