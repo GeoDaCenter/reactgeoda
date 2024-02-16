aaa05c98b36ca43b52a3a013c10952ce2cdf47ea
Xun Li <lixun910@gmail.com>
2024-02-16 10:12:44 -0700

    add test cases

--- geoda-ai/src/components/intl-provider-wrapper.tsx
+++ geoda-ai/src/components/intl-provider-wrapper.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {IntlProvider} from 'react-intl';
-// import {messages, flattenMessages} from '@kepler.gl/localization';
 
 import MESSAGES from '../translations/translations';
 import {GeoDaState} from '../store';
@@ -12,10 +11,6 @@ type IntelProviderWrapperProps = {
 
 const IntlProviderWrapper = ({children}: IntelProviderWrapperProps) => {
   const language = useSelector((state: GeoDaState) => state.root.language);
-  // const localeNames = [...Object.keys(MESSAGES), ...Object.keys(messages)];
-  // const combinedMessages = localeNames.reduce((prev, cur) => {
-  //   return {...prev, [cur]: {...MESSAGES[cur], ...messages[cur]}};
-  // }, {});
 
   return (
     <IntlProvider messages={MESSAGES[language]} locale={language}>
