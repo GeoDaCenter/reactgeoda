fb60b1e1036bdbb3a2171a9067e624c7addb1077
Xun Li <lixun910@gmail.com>
2024-04-09 15:21:42 -0700

    Update open-file-modal.tsx

--- geoda-ai/src/components/open-file-modal.tsx
+++ geoda-ai/src/components/open-file-modal.tsx
@@ -43,73 +43,53 @@ async function processDropFiles(files: File[]): Promise<ProcessDropFilesOutput> 
   return await loadDroppedFile(files);
 }
 
-const OpenFileComponent = ({projectUrl}: {projectUrl: string | null}) => {
+const OpenFileComponent = () => {
   const dispatch = useDispatch();
 
   // create a useState to store the status of loading file
   const [loading, setLoading] = useState(false);
 
-  const processFiles = async (acceptedFiles: File[]) => {
-    // show loading bar
-    setLoading(true);
+  const onDrop = useCallback(
+    async (acceptedFiles: File[]) => {
+      // show loading bar
+      setLoading(true);
 
-    // process dropped files, and return the file name, arrowTable and arrowFormatData
-    const {fileName, arrowTable, arrowFormatData, keplerConfig, geodaConfig} =
-      await processDropFiles(acceptedFiles);
+      // process dropped files, and return the file name, arrowTable and arrowFormatData
+      const {fileName, arrowTable, arrowFormatData, keplerConfig, geodaConfig} =
+        await processDropFiles(acceptedFiles);
 
-    // dispatch addDataToMap action to default kepler.gl instance
-    if (arrowFormatData) {
-      dispatch(
-        wrapTo(
-          MAP_ID,
-          addDataToMap({
-            datasets: arrowFormatData,
-            options: {centerMap: true},
-            ...(keplerConfig && {config: keplerConfig})
-          })
-        )
-      );
-    }
+      // dispatch addDataToMap action to default kepler.gl instance
+      if (arrowFormatData) {
+        dispatch(
+          wrapTo(
+            MAP_ID,
+            addDataToMap({
+              datasets: arrowFormatData,
+              options: {centerMap: true},
+              ...(keplerConfig && {config: keplerConfig})
+            })
+          )
+        );
+      }
 
-    // dispatch action to close modal, update redux state state.root.uiState.showOpenFileModal
-    dispatch(setOpenFileModal(false));
+      // dispatch action to close modal, update redux state state.root.uiState.showOpenFileModal
+      dispatch(setOpenFileModal(false));
 
-    // dispatch action to set file data, update redux state state.fileData
-    dispatch(setRawFileData({fileName, arrowTable}));
+      // dispatch action to set file data, update redux state state.fileData
+      dispatch(setRawFileData({fileName, arrowTable}));
 
-    if (geodaConfig) {
-      // dispatch action to set geoda config, update redux state state.root
-      dispatch({type: 'LOAD_PROJECT', payload: geodaConfig});
-    }
+      if (geodaConfig) {
+        // dispatch action to set geoda config, update redux state state.root
+        dispatch({type: 'LOAD_PROJECT', payload: geodaConfig});
+      }
 
-    // set loading to true to show loading bar
-    setLoading(false);
-  };
-
-  const onDrop = useCallback(processFiles, [dispatch]);
+      // set loading to true to show loading bar
+      setLoading(false);
+    },
+    [dispatch]
+  );
 
   const {getRootProps, getInputProps} = useDropzone({onDrop});
-
-  // if projectUrl presents, load the project file using fetch when the component mounts
-  useEffect(() => {
-    (async () => {
-      if (!projectUrl) return;
-      setLoading(true);
-      // add &ld=1 to the projectUrl if it is a dropbox.com link
-      const url = projectUrl.includes('dropbox.com') ? `${projectUrl}&dl=1` : projectUrl;
-      const res = await fetch(url);
-      if (!res.ok) {
-        throw new Error('Failed to fetch project file');
-      }
-      const data = res.json();
-      // create a File object from the json data
-      const file = new File([JSON.stringify(data)], 'project.geoda', {
-        type: 'application/json'
-      });
-      await processFiles([file]);
-    })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <Card>
@@ -156,15 +136,74 @@ export function OpenFileModal({projectUrl}: {projectUrl: string | null}) {
   const dispatch = useDispatch();
 
   // get the state showOpenModal from the redux store
-  const showOpenModal =
-    useSelector((state: GeoDaState) => state.root.uiState.showOpenFileModal) || Boolean(projectUrl);
+  const showOpenModal = useSelector((state: GeoDaState) => state.root.uiState.showOpenFileModal);
 
-  // create a reference to the modal for focus
-  // const modalRef = useRef(null);
+  const rawFileData = useSelector((state: GeoDaState) => state.root.file.rawFileData);
 
   const onCloseModal = () => {
     dispatch(setOpenFileModal(false));
   };
+
+  // if projectUrl presents, load the project file using fetch when the component mounts
+  useEffect(() => {
+    (async () => {
+      if (!projectUrl) return;
+      const res = await fetch(projectUrl);
+      if (!res.ok) {
+        throw new Error('Failed to fetch project file');
+      }
+      const data = await res.json();
+      // create a File object from the json data
+      const file = new File([JSON.stringify(data)], 'project.geoda', {
+        type: 'application/json'
+      });
+      // process dropped files, and return the file name, arrowTable and arrowFormatData
+      const {fileName, arrowTable, arrowFormatData, keplerConfig, geodaConfig} =
+        await processDropFiles([file]);
+      // dispatch addDataToMap action to default kepler.gl instance
+      if (arrowFormatData) {
+        dispatch(
+          wrapTo(
+            MAP_ID,
+            addDataToMap({
+              datasets: arrowFormatData,
+              options: {centerMap: true},
+              ...(keplerConfig && {config: keplerConfig})
+            })
+          )
+        );
+      }
+      // dispatch action to set file data, update redux state state.fileData
+      dispatch(setRawFileData({fileName, arrowTable}));
+      // dispatch action to set geoda config, update redux state state.root
+      if (geodaConfig) {
+        setTimeout(() => {
+          dispatch({type: 'LOAD_PROJECT', payload: geodaConfig});
+        }, 1000);
+      }
+    })();
+  }, [dispatch, projectUrl]);
+
+  if (rawFileData === null && projectUrl !== null) {
+    // showing loading project modal
+    return (
+      <Modal
+        isOpen={rawFileData === null && projectUrl !== null}
+        size="3xl"
+        placement="center"
+        className="min-w-60"
+        isDismissable={true}
+      >
+        <ModalContent>
+          <ModalHeader className="flex flex-col gap-1">Loading Project...</ModalHeader>
+          <ModalBody>
+            <Progress size="sm" isIndeterminate aria-label="Loading..." className="mt-2 max-w-md" />
+          </ModalBody>
+          <ModalFooter />
+        </ModalContent>
+      </Modal>
+    );
+  }
 
   return showOpenModal ? (
     <Modal
@@ -178,7 +217,7 @@ export function OpenFileModal({projectUrl}: {projectUrl: string | null}) {
       <ModalContent>
         <ModalHeader className="flex flex-col gap-1">Open File</ModalHeader>
         <ModalBody>
-          <OpenFileComponent projectUrl={projectUrl} />
+          <OpenFileComponent />
         </ModalBody>
         <ModalFooter />
       </ModalContent>
