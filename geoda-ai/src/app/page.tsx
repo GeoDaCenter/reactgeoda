cb2bed08e17e880aa8c7d64a25a74d09919069c3
Xun Li <lixun910@gmail.com>
2024-08-09 17:42:00 -0700

    [Feat] Update Landing page #174

--- geoda-ai/src/app/page.tsx
+++ geoda-ai/src/app/page.tsx
@@ -16,60 +16,82 @@ type SocialIconProps = Omit<IconProps, 'icon'>;
 
 const socialItems = [
   {
-    name: 'Facebook',
-    href: '#',
-    icon: (props: SocialIconProps) => <Icon {...props} icon="fontisto:facebook" />
-  },
-  {
-    name: 'Instagram',
-    href: '#',
-    icon: (props: SocialIconProps) => <Icon {...props} icon="fontisto:instagram" />
-  },
-  {
     name: 'X',
     href: '#',
     icon: (props: SocialIconProps) => <Icon {...props} icon="ri:twitter-x-fill" />
   },
   {
     name: 'GitHub',
-    href: '#',
+    href: 'https://github.com/orgs/geodaai/discussions/categories/bugs',
     icon: (props: SocialIconProps) => <Icon {...props} icon="fontisto:github" />
   },
   {
-    name: 'YouTube',
+    name: 'Slack',
     href: '#',
-    icon: (props: SocialIconProps) => <Icon {...props} icon="fontisto:youtube" />
+    icon: (props: SocialIconProps) => <Icon {...props} icon="fontisto:slack" />
+  }
+];
+
+const mainItems = [
+  {
+    image: 'img/ask-1.gif',
+    title: 'Ask Anything About Spatial Data Analysis',
+    content: [
+      'GeoDa.AI is a customized large language model that can assist users with spatial analysis tasks by responding to their text, voice and screenshot prompts. The model is fine-tuned on a corpus of spatial data and spatial analysis tasks, and it can understand and respond to a variety of natural language queries.',
+      'You can ask anything about your data and apply spatial data analysis using GeoDa.AI. GeoDa AI can run spatial statistics, generate maps and charts, create spatial weights and even apply spatial regressions to help you understand your spatial data.'
+    ]
+  },
+  {
+    image: 'img/image-1.png',
+    title: 'Get Insights With Powerful Tools',
+    content: [
+      'GeoDa provides powerful tools for spatial data analysis, including mapping, space-time exploration, spatial weights, spatial autocorrelation, spatial cluster analysis, and spatial regression. GeoDa.AI will leverage these tools in conjunction with the AI model to enable users to gain insights from their spatial data and solve spatial problems more quickly and easily.',
+      'In comparison to traditional spatial analysis tools, the GeoDa.AI model is capable of learning and improving itself from the spatial analyses that all users perform. As the model is used by more and more users, it will continue to learn and improve, making it an increasingly powerful and valuable tool for spatial analysis.'
+    ]
+  },
+  {
+    image: 'img/interactive-map-1.gif',
+    title: 'Interactive Spatial Data Analysis',
+    content: [
+      'GeoDa.AI extends the classic brushing-and-linking functionality of desktop GeoDa to the browser. It enables users to perform interactive spatial data analysis across maps, tables, plots, and chats. GeoDa.AI also supports SQL queries on browser-based data. This provides a novel and convenient way to explore spatial data.',
+      'GeoDa.AI is designed and developed based on libgeoda, Arrow, DuckDB, WebAssembly, and Kepler.GL. This enables us to run the entire GeoDa library in the browser quickly and efficiently.'
+    ]
+  },
+  {
+    image: 'img/image-4.png',
+    title: 'Share Your Analysis Dashboard With Style',
+    content: [
+      'A good dashboard helps you to visualize data, communicate results, and collaborate with others. GeoDa.AI makes creating and sharing a dashboard so easy: you can drag-n-drop any text, plot and map from your chat with GeoDa.AI into the dashboard. They can also use the text editor to add titles, labels, and annotations. When users are finished creating their dashboard, they can share it with others by simply sharing the dashboard file.'
+    ]
   }
 ];
 
 export default function Page() {
   const [showSignUp, setShowSignUp] = useState(false);
   return (
-    <div className="flex w-screen items-center justify-center bg-gradient-to-br from-black via-rose-950 to-black p-2 sm:p-4 lg:p-2">
-      <div className="flex w-full max-w-full flex-col gap-4 p-10">
-        <div className="relative m-auto mb-40 mt-20 inline-flex items-start gap-10">
-          <div className="relative mt-10 inline-flex flex-col items-start gap-10">
-            <div className="relative inline-flex items-start gap-4">
+    <div className="flex w-screen items-center justify-center bg-gradient-to-br from-black via-rose-950 to-black">
+      <div className="flex w-full max-w-full flex-col gap-4">
+        <div className="mt-10 flex flex-col items-center justify-center gap-2 p-4 md:mt-20 md:gap-8 xl:mb-40 xl:flex-row">
+          <div className="flex flex-col items-center gap-4 xl:ml-6 xl:items-start">
+            <div className="flex flex-row items-start gap-4">
               <h2 className="inline bg-gradient-to-br from-foreground-800 to-foreground-200 bg-clip-text text-7xl font-semibold tracking-tight text-transparent dark:to-foreground-200">
                 GeoDa.AI
               </h2>
               <img className="relative" alt="Geoda ai logo" src="img/geoda-ai-logo.png" />
             </div>
-            <h2 className="inline max-w-[500px] bg-gradient-to-br from-foreground-800 to-foreground-200 bg-clip-text text-4xl font-normal leading-relaxed text-slate-50 dark:to-foreground-200">
+            <h2 className="max-w-[500px] bg-gradient-to-br from-foreground-800 to-foreground-200 bg-clip-text pl-2 text-4xl font-normal leading-relaxed text-slate-50 dark:to-foreground-200">
               Free, Open-Sourced and AI Powered Spatial Data Analysis
             </h2>
-            <div className="flex flex-row gap-4">
-              <Button
-                radius="lg"
-                className="bg-gradient-to-tr from-pink-500 to-yellow-500 p-6 text-large text-white shadow-lg"
-                endContent={<Icon icon="akar-icons:arrow-right" />}
-                onClick={() => setShowSignUp(true)}
-              >
-                Try Techinical Preview
-              </Button>
-            </div>
+            <Button
+              radius="lg"
+              className="bg-gradient-to-tr from-pink-500 to-yellow-500 p-6 text-large text-white shadow-lg"
+              endContent={<Icon icon="akar-icons:arrow-right" />}
+              onClick={() => setShowSignUp(true)}
+            >
+              Try Techinical Preview
+            </Button>
           </div>
-          <div className="relative h-[441px] w-[828px]">
+          <div className="scale-[0.4] items-start md:scale-100">
             <div className="relative top-[5px] h-[436px] w-[810px]">
               <div className="absolute left-[51px] top-0 h-[421px] w-[726px] rounded-lg bg-[#717171]"></div>
               <img
@@ -86,86 +108,29 @@ export default function Page() {
           </div>
         </div>
         <div className="main-content flex flex-col gap-10 text-stone-800 dark:text-stone-300">
-          <div className="backdrop-saturate-5 m-auto flex flex-row gap-10 rounded-2xl  bg-background/60 p-10 shadow-xl backdrop-blur-xl backdrop-saturate-200">
-            <div className="w-[360px] gap-3 rounded-xl">
-              <img className="w-[360px] rounded-xl" alt="Img intro" src="img/ask-1.gif" />
+          {mainItems.map((item, i) => (
+            <div
+              key={i}
+              className="backdrop-saturate-5 m-2 flex flex-col-reverse items-center gap-10 rounded-2xl bg-background/60 shadow-xl backdrop-blur-xl backdrop-saturate-200 md:m-auto md:flex-row md:items-start md:p-10"
+            >
+              <div className="min-w-[360px] max-w-[360px] gap-3 rounded-xl">
+                <img className="rounded-xl" alt="Img intro" src={item.image} />
+              </div>
+              <div className="flex max-w-[700px] flex-col gap-10 p-2">
+                <p className="text-4xl">{item.title}</p>
+                {item.content.map((content, i) => (
+                  <span key={i} className="leading-relaxed">
+                    {content}
+                  </span>
+                ))}
+              </div>
             </div>
-            <div className="flex w-[800px] flex-col gap-10">
-              <p className="text-4xl">Ask Anything About Spatial Data Analysis</p>
-              <span className="leading-relaxed">
-                GeoDa.AI is a customized large language model that can assist users with spatial
-                analysis tasks by responding to their text, voice and screenshot prompts. The model
-                is fine-tuned on a corpus of spatial data and spatial analysis tasks, and it can
-                understand and respond to a variety of natural language queries.
-              </span>
-              <span className="leading-relaxed">
-                You can ask anything about your data and apply spatial data analysis using GeoDa.AI.
-                GeoDa AI can run spatial statistics, generate maps and charts, create spatial
-                weights and even apply spatial regressions to help you understand your spatial data.
-              </span>
-            </div>
-          </div>
-          <div className="m-auto flex flex-row gap-10 rounded-2xl bg-background/60 p-10 shadow-small backdrop-blur-md backdrop-saturate-200">
-            <div className="w-[360px] gap-3 rounded-xl">
-              <img className="rounded-xl" alt="Img intro" src="img/image-1.png" />
-            </div>
-            <div className="flex w-[800px] flex-col gap-10">
-              <p className="text-4xl">Get Insights With Powerful Tools</p>
-              <span className="leading-relaxed">
-                GeoDa provides powerful tools for spatial data analysis, including mapping,
-                space-time exploration, spatial weights, spatial autocorrelation, spatial cluster
-                analysis, and spatial regression. GeoDa.AI will leverage these tools in conjunction
-                with the AI model to enable users to gain insights from their spatial data and solve
-                spatial problems more quickly and easily.
-              </span>
-              <span className="leading-relaxed">
-                In comparison to traditional spatial analysis tools, the GeoDa.AI model is capable
-                of learning and improving itself from the spatial analyses that all users perform.
-                As the model is used by more and more users, it will continue to learn and improve,
-                making it an increasingly powerful and valuable tool for spatial analysis.
-              </span>
-            </div>
-          </div>
-          <div className="m-auto flex flex-row gap-10 rounded-2xl bg-background/60 p-10 shadow-small backdrop-blur-md backdrop-saturate-200">
-            <div className="w-[360px] gap-3 rounded-xl">
-              <img className="rounded-xl" alt="Img intro" src="img/interactive-map-1.gif" />
-            </div>
-            <div className="flex w-[800px] flex-col gap-10">
-              <p className="text-4xl">Interactive Spatial Data Analysis</p>
-              <span className="leading-relaxed">
-                GeoDa.AI extends the classic brushing-and-linking functionality of desktop GeoDa to
-                the browser. It enables users to perform interactive spatial data analysis across
-                maps, tables, plots, and chats. GeoDa.AI also supports SQL queries on browser-based
-                data. This provides a novel and convenient way to explore spatial data.
-              </span>
-              <span className="leading-relaxed">
-                GeoDa.AI is designed and developed based on libgeoda, Arrow, DuckDB, WebAssembly,
-                and Kepler.GL. This enables us to run the entire GeoDa library in the browser
-                quickly and efficiently.
-              </span>
-            </div>
-          </div>
-          <div className="m-auto flex flex-row gap-10 rounded-2xl bg-background/60 p-10 shadow-small backdrop-blur-md backdrop-saturate-200">
-            <div className="w-[360px] gap-3 rounded-xl">
-              <img className="rounded-xl" alt="Img intro" src="img/image-4.png" />
-            </div>
-            <div className="flex w-[800px] flex-col gap-10">
-              <p className="text-4xl">Share Your Analysis Dashboard With Style</p>
-              <span className="leading-relaxed">
-                A good dashboard helps you to visualize data, communicate results, and collaborate
-                with others. GeoDa.AI makes creating and sharing a dashboard so easy: you can
-                drag-n-drop any text, plot and map from your chat with GeoDa.AI into the dashboard.
-                They can also use the text editor to add titles, labels, and annotations. When users
-                are finished creating their dashboard, they can share it with others by simply
-                sharing the dashboard file.
-              </span>
-            </div>
-          </div>
+          ))}
         </div>
         <footer className="flex w-full flex-col">
-          <div className="mx-auto flex w-full max-w-7xl flex-col items-center justify-center px-6 py-12 lg:px-8">
+          <div className="flex w-full max-w-7xl flex-col items-center justify-center px-6 py-12 lg:px-8">
             <div className="flex items-center justify-center gap-4">
-              <img className="relative" alt="Geoda ai logo" src="img/geoda-ai-logo.png" />
+              <img className="" alt="Geoda ai logo" src="img/geoda-ai-logo.png" />
               <span className="text-2xl font-semibold text-stone-50 dark:text-stone-500">
                 GeoDa.AI
               </span>
@@ -187,14 +152,16 @@ export default function Page() {
         </footer>
       </div>
       <Modal
-        size="2xl"
+        size="lg"
         isOpen={showSignUp}
         isDismissable={true}
         onClose={() => setShowSignUp(false)}
         backdrop="blur"
       >
         <ModalContent>
-          <ModalHeader className="flex flex-col gap-1"></ModalHeader>
+          <ModalHeader className="flex flex-col gap-1">
+            <Icon icon="twemoji:building-construction" width="1.2em" height="1.2em" />
+          </ModalHeader>
           <ModalBody>
             <SignUpButton />
           </ModalBody>
--- geoda-ai/src/components/chat/prompt-input-with-bottom-actions.tsx
+++ geoda-ai/src/components/chat/prompt-input-with-bottom-actions.tsx
@@ -89,12 +89,15 @@ export default function Component({
     err => console.table(err) // onNotAllowedOrFound
   );
 
-  const onTalkEnd = async () => {
-    recorderControls.stopRecording();
-
-    if (recorderControls.recordingBlob) {
-      const voice = await onVoiceMessage(recorderControls.recordingBlob);
-      setPrompt(voice);
+  const onTalkClicked = async () => {
+    if (recorderControls.isRecording) {
+      recorderControls.stopRecording();
+      if (recorderControls.recordingBlob) {
+        const voice = await onVoiceMessage(recorderControls.recordingBlob);
+        setPrompt(voice);
+      }
+    } else {
+      recorderControls.startRecording();
     }
   };
 
@@ -199,36 +202,43 @@ export default function Component({
               startContent={
                 <Icon className="text-default-500" icon="solar:soundwave-linear" width={18} />
               }
-              spinner={
-                <svg
-                  className="h-4 w-4 animate-spin text-current"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <circle className="opacity-75" cx="12" cy="12" r="4" strokeWidth="0" fill="red" />
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="1"
-                  />
-                  <path
-                    className="opacity-50"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    fill="currentColor"
-                    strokeWidth="2"
-                  />
-                </svg>
+              endContent={
+                recorderControls.isRecording && (
+                  <svg
+                    className="h-4 w-4 animate-spin text-current"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle
+                      className="opacity-75"
+                      cx="12"
+                      cy="12"
+                      r="4"
+                      strokeWidth="0"
+                      fill="red"
+                    />
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="1"
+                    />
+                    <path
+                      className="opacity-50"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      fill="currentColor"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                )
               }
-              isLoading={recorderControls.isRecording}
               variant="flat"
-              onPressStart={recorderControls.startRecording}
-              onPressEnd={onTalkEnd}
+              onClick={onTalkClicked}
             >
-              Hold Down & Talk to Ask
+              Talk to Ask
             </Button>
             {enableAttachFile && (
               <Button
