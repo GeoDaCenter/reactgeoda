da267335aa0d7d2d3d50461f1aa2a60b10c9d745
Xun Li <lixun910@gmail.com>
2024-10-26 12:10:10 -0700

    [Fix] failed to load duckdb wasm sometimes

--- geoda-ai/src/app/preview/page.tsx
+++ geoda-ai/src/app/preview/page.tsx
@@ -9,10 +9,10 @@ import '@/styles/style.css';
 import '@/styles/superfine.css';
 import '@/styles/maplibre-gl.css';
 
-import {useEffect, useRef} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import {Provider as ReduxProvider} from 'react-redux';
 import {RootContext} from '@kepler.gl/components';
-
+import {Icon} from '@iconify/react';
 import store from '@/store';
 import IntlProviderWrapper from '@/components/intl-provider-wrapper';
 import ThemeProviderWrapper from '@/components/theme-provider-wrapper';
@@ -25,17 +25,30 @@ const MainConatiner = dynamic(() => import('@/components/main-container'), {ssr:
 
 export default function Home() {
   const rootNode = useRef<HTMLDivElement>(null);
-
   const searchParams = useSearchParams();
-
   const projectUrl = searchParams.get('project');
+  const [isDuckDBReady, setIsDuckDBReady] = useState(false);
 
   useGeoDa();
 
-  // run initDuckDB before rendering the app
+  // Initialize DuckDB and set ready state
   useEffect(() => {
-    DuckDB.getInstance().initDuckDB();
+    async function initDuckDB() {
+      await DuckDB.getInstance().initDuckDB();
+      setIsDuckDBReady(true);
+    }
+    initDuckDB();
   }, []);
+
+  if (!isDuckDBReady) {
+    return (
+      <div
+        style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}
+      >
+        <Icon icon="eos-icons:three-dots-loading" width="120" height="120" />
+      </div>
+    );
+  }
 
   return (
     <RootContext.Provider value={rootNode}>
