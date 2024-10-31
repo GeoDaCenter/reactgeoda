b21530852291511041b492378106c46cc421887c
Xun Li <lixun910@gmail.com>
2024-10-31 09:26:28 -0700

    [Preview] show selected indep variables

--- geoda-ai/src/components/common/multivariable-selector.tsx
+++ geoda-ai/src/components/common/multivariable-selector.tsx
@@ -1,6 +1,6 @@
 import {selectKeplerDataset} from '@/store/selectors';
 import {getNumericFieldNamesFromDataset} from '@/utils/data-utils';
-import {Listbox, ListboxItem, ScrollShadow} from '@nextui-org/react';
+import {Chip, Listbox, ListboxItem, ScrollShadow} from '@nextui-org/react';
 import {useMemo, useState} from 'react';
 import {useSelector} from 'react-redux';
 
@@ -16,7 +16,7 @@ type MultiVariableSelectorProps = {
 export function MultiVariableSelector(props: MultiVariableSelectorProps) {
   const {datasetId, label, isInvalid, setVariables, excludeVariables} = props;
   const [selectedKeys, setSelectedKeys] = useState(new Set([]));
-
+  const selectedVariables = Array.from(selectedKeys);
   const keplerDataset = useSelector(selectKeplerDataset(datasetId));
 
   // get numeric columns from redux store
@@ -34,11 +34,30 @@ export function MultiVariableSelector(props: MultiVariableSelectorProps) {
     setVariables(Array.from(keys));
   }
 
+  const topContent = useMemo(() => {
+    if (!selectedVariables.length) {
+      return null;
+    }
+
+    return (
+      <ScrollShadow
+        hideScrollBar
+        className="flex w-full gap-1 px-2 py-0.5"
+        orientation="horizontal"
+      >
+        {selectedVariables.map(value => (
+          <Chip key={value}>{value}</Chip>
+        ))}
+      </ScrollShadow>
+    );
+  }, [selectedVariables.length]);
+
   return (
     <div className="flex w-full flex-col gap-2">
       <span className={`m-2 text-sm  ${isInvalid ? 'text-danger' : ''}`}>
         {label || 'Select variables'}
       </span>
+      {topContent}
       <ScrollShadow
         className={`${isInvalid ? 'bg-danger-50' : ''} h-[200px] w-full overflow-y-scroll rounded-small border-small border-default-200 px-1 py-2 dark:border-default-100`}
       >
