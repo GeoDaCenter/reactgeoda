3a7a30ae234384767cd50c1803b951d0c3434020
Xun Li <lixun910@gmail.com>
2025-02-26 12:19:34 -0700

    fix lint [Preview]

--- geoda-ai/src/hooks/use-dataset-fields.ts
+++ geoda-ai/src/hooks/use-dataset-fields.ts
@@ -1,6 +1,6 @@
 import {useMemo} from 'react';
 import {useSelector} from 'react-redux';
-import {datasetsSelector, defaultDatasetIdSelector, selectKeplerDataset} from '@/store/selectors';
+import {datasetsSelector, selectKeplerDataset} from '@/store/selectors';
 import {
   getIntegerFieldNamesFromDataset,
   getIntegerAndStringFieldNamesFromDataset,
